bec368a43afa2e08a829ecd84716fd7d3146a1e7
KThompson <kt3223@gmail.com>
2018-11-12 17:07:06 +0000

    Integration tests added

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,17 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe package("openjdk-8-jdk") do
+  it {should be_installed}
+  its('version') {should match "8u181-b13-1ubuntu0.16.04.1"}
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe package("jenkins") do
+  it {should be_installed}
+  its('version') { should match /2\.1/ }
+end
+
+describe service("jenkins") do
+  it {should be_running}
+  it {should be_enabled}
 end
